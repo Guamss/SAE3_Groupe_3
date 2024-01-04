06ab4bac6e937be80097c4340e427bf5dc5a7b04
EliasMoussamih <128966201+EliasMoussamih@users.noreply.github.com>
2024-01-04 19:04:23 +0100

    Complétion du script de création et ajout des contraintes / procédures SQL

--- src/script/creation_BD.sql
+++ src/script/creation_BD.sql
@@ -1,15 +1,17 @@
-USE SAE3;
+CREATE database SAE3_groupe3;
+USE SAE3_groupe3;
 
-CREATE TABLE `User` (
+-- creation des tables
+CREATE TABLE User (
     UID INT AUTO_INCREMENT PRIMARY KEY,
-    role ENUM('user', 'technician', 'webadmin', 'sysadmin') NOT NULL,
+    role ENUM('user', 'technician', 'webadmin', 'sysadmin') DEFAULT 'user',
     login VARCHAR(50) UNIQUE,
     password VARCHAR(60) NOT NULL
 );
 
 CREATE TABLE Label (
     Label_ID INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
+    name VARCHAR(100),
     archivé TINYINT(1) DEFAULT 0
 );
 
@@ -17,12 +19,163 @@ CREATE TABLE Ticket (
     Ticket_ID INT AUTO_INCREMENT PRIMARY KEY,
     UID INT,
     Technician_ID INT,
-    urgence_level ENUM('1', '2', '3', '4'),
     Label_ID INT,
-    creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    urgence_level ENUM('1','2','3','4'),
+    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status ENUM('Ouvert', 'En Cours', 'Fermé') DEFAULT 'Ouvert',
     description TEXT,
-    FOREIGN KEY (UID) REFERENCES `User`(UID),
-    FOREIGN KEY (Technician_ID) REFERENCES `User`(UID),
+    FOREIGN KEY (UID) REFERENCES User(UID),
+    FOREIGN KEY (Technician_ID) REFERENCES User(UID),
     FOREIGN KEY (Label_ID) REFERENCES Label(Label_ID)
 );
+
+
+
+-- ajout des contraintes
+ALTER TABLE User
+ADD CONSTRAINT unique_login UNIQUE (login);
+
+-- ajout des contraintes
+ALTER TABLE User
+ADD CONSTRAINT unique_login UNIQUE (login);
+
+-- ajout des procédures
+
+-- ajouter un utilisateur
+DELIMITER $$
+CREATE PROCEDURE Insert_User(
+    IN p_login VARCHAR(50),
+    IN p_password VARCHAR(50)
+)
+BEGIN
+    IF LENGTH(p_password) >= 8 and LENGTH(p_password) <= 30 THEN
+        INSERT INTO User (login, password)
+        VALUES (p_login, p_password);
+    ELSE
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Le mot de passe doit avoir au moins 8 caractères.';
+    END IF;
+END $$
+DELIMITER ;
+
+-- regarder les tickets qui ont été crées pour un utilisateur 
+DELIMITER $$
+CREATE PROCEDURE UserTickets(
+	IN userID INT
+)
+BEGIN
+    SET @query = CONCAT('SELECT t.Ticket_ID, t.Label_ID, t.urgence_level, t.creation_date, t.status, t.description
+                         FROM Ticket t
+                         INNER JOIN User u ON t.UID = u.UID
+                         WHERE u.UID = ', userID);
+    PREPARE statement FROM @query;
+    EXECUTE statement;
+    DEALLOCATE PREPARE statement;
+END $$
+DELIMITER ;
+
+-- Ouvrir un ticket
+DELIMITER $$
+CREATE PROCEDURE AddTicket(
+    IN p_UID INT,
+    IN p_Label_ID INT,  
+    IN p_urgence_level INT,
+    IN p_description TEXT
+)
+BEGIN
+    DECLARE user_exists INT;
+
+    SELECT COUNT(*) INTO user_exists FROM User WHERE UID = p_UID;
+
+    IF p_urgence_level BETWEEN 1 AND 4 AND user_exists = 1 THEN
+        INSERT INTO Ticket (UID, Label_ID, urgence_level, description)
+        VALUES (p_UID, p_Label_ID, p_urgence_level, p_description);
+    ELSE
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Les informations données ne sont pas correct';
+    END IF;
+END $$
+DELIMITER ;
+
+-- Ajouter un label
+DELIMITER $$
+CREATE PROCEDURE AddLabel(
+    IN p_name TEXT
+)
+BEGIN
+        INSERT INTO Label (name)
+        VALUES (p_name);
+END $$
+DELIMITER ;
+
+-- fermer un ticket
+DELIMITER $$
+
+CREATE PROCEDURE DeleteTicket(
+    IN p_UID INT,
+    IN p_TicketID INT
+)
+BEGIN
+    DECLARE ticket_exists INT;
+    DECLARE user_good INT;
+
+    SELECT COUNT(*) INTO ticket_exists FROM Ticket WHERE Ticket_ID = p_TicketID;
+    SELECT COUNT(*) INTO user_good FROM Ticket WHERE Ticket_ID = p_TicketID AND UID = p_UID;
+
+    IF ticket_exists > 0 AND user_good > 0 THEN
+        DELETE FROM Ticket WHERE Ticket_ID = p_TicketID;
+    ELSE
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Les informations données ne sont pas correct';
+    END IF;
+END $$
+DELIMITER ;
+
+-- Procédure pour afficher les tickets créés au cours des 7 derniers jours
+DELIMITER $$
+CREATE PROCEDURE TicketsCreatedLast7Days()
+BEGIN
+    SELECT Ticket_ID, UID, Technician_ID, Label_ID, urgence_level, creation_date, status, description
+    FROM Ticket
+    WHERE creation_date >= CURDATE() - INTERVAL 7 DAY;
+END $$
+DELIMITER ;
+
+-- procedure permettant d'assigner un technicien a un ticket 
+DELIMITER $$
+
+CREATE PROCEDURE AssignTechnicianToTicket(
+    IN p_TicketID INT,
+    IN p_UID INT
+)
+BEGIN
+    DECLARE is_technician INT;
+
+    SELECT COUNT(*) INTO is_technician FROM User WHERE UID = p_UID AND role = 'technician';
+
+    IF is_technician > 0 THEN
+        UPDATE Ticket SET Technician_ID = p_UID WHERE Ticket_ID = p_TicketID;
+    ELSE
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'L\'utilisateur n\'a pas le rôle de technicien ou n\' existe pas';
+    END IF;
+END $$
+
+DELIMITER ;
+
+-- Procédure permettant de changer le status d'un ticket
+DELIMITER $$
+
+CREATE PROCEDURE UpdateTicketStatus(
+    IN p_TicketID INT,
+    IN p_NewStatus ENUM('Ouvert', 'En Cours', 'Fermé')
+)
+BEGIN
+    DECLARE ticket_exists INT;
+
+    SELECT COUNT(*) INTO ticket_exists FROM Ticket WHERE Ticket_ID = p_TicketID;
+
+    IF ticket_exists > 0 THEN
+        UPDATE Ticket SET status = p_NewStatus WHERE Ticket_ID = p_TicketID;
+    ELSE
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Le ticket spécifié n\'existe pas.';
+    END IF;
+END $$
+
+DELIMITER ;
